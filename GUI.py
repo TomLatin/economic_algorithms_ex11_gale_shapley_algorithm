751601a51334edaf805e620fc92eabeaf6342f40
TomLatin <tomlatin52@gmail.com>
2021-01-02 16:12:13 +0200

    need to add doctest

--- GUI.py
+++ GUI.py
@@ -1,32 +1,50 @@
+import os
 import tkinter as tk
 from tkinter import font as tkfont, filedialog
 
-from gale_and_shapley_algorithm import init_dicts, activate_all_func
+from gale_and_shapley_algorithm import init_dicts, activate_all_func, tentative_acceptance
 
-
-class  SampleApp(tk.Tk):
+class SampleApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.iconbitmap(self, default="student.ico")
         tk.Tk.wm_title(self, "Gale&Shapley Algorithm")
-        self.geometry("400x200")
+        self.geometry("500x500")
         self.title_font = tkfont.Font(family='Times', size=18, weight="bold", slant="italic")
         def file_dialog():
             global name_file
             name_file = filedialog.askopenfilename(filetypes=(('text files', 'txt'),))
+            add_file.config(text=os.path.basename(name_file))
+            """
+            >>> name_file="dict.txt"
+            'Answer\nryan- Yale\n blake- Harvard\njosh- MIT\nconnor- NYU\n'
+            
+            """
+
+        def print_ans_using_global():
+            ans = "Answer: \n"
+            for i in range(len(tentative_acceptance)):
+                ans += str(tentative_acceptance[i]).replace("'", '').replace('[', '').replace(']', '').replace(',','-')+"\n"
+            font_style = tkfont.Font(family="Lucida Grande", size=20)
+            ans_label = tk.Label(self, text=ans, font=font_style)
+            ans_label.grid(row=12, column=0, columnspan=2, pady=10, padx=40, ipadx=100)
+            main.after(6000, main.destroy)
 
         # Create Text Box
         add_file = tk.Button(self, text="Brows A File", command=lambda: file_dialog())
-        add_file.grid(row=2, column=1, pady=20, padx=10)
+        add_file.grid(row=2, column=1, pady=20, padx=10,ipadx=50)
 
         # Creat Text Box Labels
         add_file_label = tk.Label(self, text="Open A File:")
         add_file_label.grid(row=2, column=0, pady=20, padx=10)
 
         # Creat Submit Button
-        submit_btn = tk.Button(self, text="Submit",command=lambda: [init_dicts(name_file),activate_all_func()])
+        submit_btn = tk.Button(self, text="Submit",command=lambda: [init_dicts(name_file), activate_all_func(), print_ans_using_global()])
         submit_btn.grid(row=10, column=0, columnspan=2, pady=50, padx=40, ipadx=100)
 
 if __name__ == "__main__":
+    import doctest
+    (failures, tests) = doctest.testmod(report=True)
+    print("{} failures, {} tests".format(failures, tests))
     main = SampleApp()
     main.mainloop()
--- gale_and_shapley_algorithm.py
+++ gale_and_shapley_algorithm.py
@@ -1,33 +1,24 @@
 import collections
 import doctest
+import os  # For check the path
 
-# The university that the student prefer
-preferred_rankings_student = {
-    'ryan': ['Yale', 'Harvard', 'NYU', 'MIT'],
-    'josh': ['Harvard', 'Yale', 'MIT', 'NYU'],
-    'blake': ['Harvard', 'MIT', 'NYU', 'Yale'],
-    'connor': ['Yale', 'Harvard', 'NYU', 'MIT']
-}
-
-# The student that the university prefer
-preferred_rankings_university = {
-    'Yale': ['ryan', 'blake', 'josh', 'connor'],
-    'Harvard': ['ryan', 'blake', 'connor', 'josh'],
-    'NYU': ['connor', 'josh', 'ryan', 'blake'],
-    'MIT': ['ryan', 'josh', 'connor', 'blake']
-}
-
+preferred_rankings_student = {}
+preferred_rankings_university = {}
 
 free_student = []
 tentative_acceptance = []
 
 '''Initializes the students'''
+
+
 def init_free_student() -> None:
     for student in preferred_rankings_student.keys():
         free_student.append(student)
 
 
 '''Matching algorithm until stable match terminates'''
+
+
 def stable_matching() -> None:
     while len(free_student) > 0:
         for student in free_student:
@@ -35,6 +26,8 @@ def stable_matching() -> None:
 
 
 '''Find the first free woman available to a man at any given time'''
+
+
 def begin_matching(student: str) -> None:
     print("DEALING WITH %s" % student)
     for university in preferred_rankings_student[student]:
@@ -59,7 +52,8 @@ def begin_matching(student: str) -> None:
                 print('She\'s satisfied with %s..' % (taken_match[0][0]))
             else:
                 print('%s is better than %s' % (student, taken_match[0][0]))
-                print('Making %s free again.. and tentatively engaging %s and %s' % (taken_match[0][0], student, university))
+                print('Making %s free again.. and tentatively engaging %s and %s' % (
+                    taken_match[0][0], student, university))
 
                 # The new student is acceptance
                 free_student.remove(student)
@@ -72,7 +66,51 @@ def begin_matching(student: str) -> None:
                 break
 
 
-if __name__ == "__main__":
+def init_dicts(path: str):
+    have_dict_student = False
+    have_dict_university = False
+    if os.path.exists(path):  # Path is good
+        with open(path) as file:
+
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                if have_dict_student and have_dict_university:
+                    break
+                if "student" in line or "Student" in line:
+                    have_dict_student = True
+                    line = file.readline()
+                    while line != "\n":
+                        # get key
+                        key = line.split(":")[0].strip()
+                        # get value
+                        preference_student_first = line.split(":")[1].strip(' [ ]\n')
+                        preference_student_second = preference_student_first.split(",")
+                        preference_student_list = []
+                        for x in range(len(preference_student_second)):
+                            preference_student_list.append(preference_student_second[x])
+                        preferred_rankings_student.update({key: preference_student_list})
+                        line = file.readline()
+                if "university" in line or "University" in line:
+                    have_dict_university = True
+                    line = file.readline()
+                    while line != "\n":
+                        if line == '':
+                            break
+                        # get key
+                        key = line.split(":")[0].strip()
+                        # get value
+                        preference_university_first = line.split(":")[1].strip(' [ ]\n')
+                        preference_university_second = preference_university_first.split(",")
+                        preference_university_list = []
+                        for x in range(len(preference_university_second)):
+                            preference_university_list.append(preference_university_second[x])
+                        preferred_rankings_university.update({key: preference_university_list})
+                        line = file.readline()
+
+
+def activate_all_func():
     init_free_student()
     print(free_student)
     stable_matching()
